6603033c1c79571d25f8afd8894d895d47f66b4c
Paula Contreras Nino <pauladc@mit.edu>
2022-06-18 18:03:03 -0700

    added native base

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { NativeBaseProvider } from 'native-base';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -13,10 +13,12 @@ export default function App() {
     return null;
   } else {
     return (
+      <NativeBaseProvider>
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
+      </NativeBaseProvider>
     );
   }
 }
