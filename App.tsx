1626a5603b1b6d721edeb5a18125982b183be710
Paula Contreras Nino <pauladc@mit.edu>
2022-06-19 00:17:42 -0700

    added signin

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import Navigation from './navigation';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,38 +19,29 @@ const firebaseConfig = {
   projectId: "fillourneed-80b86",
   storageBucket: "fillourneed-80b86.appspot.com",
   messagingSenderId: "612459122798",
-  appId: "1:612459122798:web:0025de3fe6d6c98a6a567f"
+  appId: "1:612459122798:web:0025de3fe6d6c98a6a567f",
+  databaseURL: "https://fillourneed-80b86-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-const provider = new GoogleAuthProvider();
-provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-const auth = getAuth();
-auth.languageCode = 'it';
+initializeApp(firebaseConfig);
+// const database = getDatabase(app);
 
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
+// import { ref, set } from "firebase/database";
 
-    if (credential != null) {
-    const token = credential.accessToken;
-    }
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+// function writeUserData( { name }: { name: string }, { est_time }: { est_time: number }, { mission_desc }: { mission_desc: string }) {
+//   const db = getDatabase()
+//   const reference = ref(db)
+
+//   set(reference, 
+//       {username: name,
+//       time: est_time,
+//       mission : mission_desc
+//     });
+// }
+
+// writeUserData('paula', 60, 'get lost cat');
+
 
 
 export default function App() {
--- components/EditProfile.tsx
+++ components/EditProfile.tsx
@@ -1,43 +1,51 @@
 import * as WebBrowser from 'expo-web-browser';
 import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Box, Button, View } from 'native-base'
+import { initializeApp } from "firebase/app";
 
-import Colors from '../constants/Colors';
-import { MonoText } from './StyledText';
-import { Text, View } from './Themed';
+const firebaseConfig = {
+  apiKey: "AIzaSyDFlq4ModItzY4I-yK7V4vtMO3TlwPtJuE",
+  authDomain: "fillourneed-80b86.firebaseapp.com",
+  projectId: "fillourneed-80b86",
+  storageBucket: "fillourneed-80b86.appspot.com",
+  messagingSenderId: "612459122798",
+  appId: "1:612459122798:web:0025de3fe6d6c98a6a567f",
+  databaseURL: "https://fillourneed-80b86-default-rtdb.firebaseio.com"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+auth.languageCode = 'it';
+
+const signin = signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+
+    if (credential != null) {
+    const token = credential.accessToken;
+    }
+    const user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+  });
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
-      <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
-        </View>
-
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
-        </Text>
-      </View>
-
-      <View style={styles.helpContainer}>
-        <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <Box alignItems="center">
+      <Button onPress={() => signin }> Sign in! </Button>
+    </Box>
     </View>
   );
 }
